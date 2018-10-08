cdd3f19df90eb394c43f315dc4799467c7cabc8a
bjelinek26 <brad.jelinek.226@gmail.com>
2018-10-08 12:37:58 -0400

    On branch master  Initial commit  Changes to be committed: 	new file:   README 	new file:   youtube_mp3.sh

---
+++ youtube_mp3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+FOLD=$1
+
+mkdir $FOLD
+
+cd $FOLD
+
+touch list.sh
+
+#read -p "paste a link (type "stop" when done) " LINK
+
+
+while [ "$LINK" != "stop" ]
+do
+	read -p "paste a link (type "stop" when done) " LINK
+
+	if [ "$LINK" != "stop" ]
+	then
+		echo $LINK >> list.sh
+	fi
+done  
+
+youtube-dl -x --audio-format mp3 -a list.sh
+
